21563480c1dc9d194f96fb889552ed264b09ee10
guneyumit <91600221+guneyumit@users.noreply.github.com>
2023-09-22 18:41:43 +0300

    Updated

--- src/test/java/com/cybertek/tests/day7_testng/TestNGAssertionsDemo.java
+++ src/test/java/com/cybertek/tests/day7_testng/TestNGAssertionsDemo.java
@@ -4,8 +4,6 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import com.cybertek.utilities.ConfigurationReader;
-import com.cybertek.utilities.Driver;
 
 public class TestNGAssertionsDemo {
 
